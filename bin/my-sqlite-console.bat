6a0a28fac29d4d4ab7e84113590874d8699bdc8f
petterobam <1460300366@qq.com>
2018-03-12 23:43:53 +0800

    添修shell脚本备注；添加windows启动脚本

---
+++ bin/my-sqlite-console.bat
@@ -0,0 +1,2 @@
+chcp 65001
+java -jar ..\my-sqlite-console-1.0-SNAPSHOT.jar >> my-sqlite-console.log 2>> my-sqlite-console-error.log
--- bin/my-sqlite-console.sh
+++ bin/my-sqlite-console.sh
@@ -25,13 +25,16 @@ startup(){
     pid=$(get_pid)
     if [ "$pid" != "" ]
     then
-        echo "Tale already startup!"
+        echo "my-sqlite-console already startup!"
     else
         jar_path=`find .. -name 'my-sqlite-console*.jar'`
         echo "jarfile=$jar_path"
         cmd="java $1 -jar $jar_path > ./my-sqlite-console.out < /dev/null &"
         echo "cmd: $cmd"
         java $1 -jar $jar_path > ./my-sqlite-console.out < /dev/null &
+        echo "---------------------------------"
+        echo "启动完成，按CTRL+C退出日志界面即可>>>>>"
+        echo "---------------------------------"
         show_log
     fi
 }
@@ -41,9 +44,9 @@ shut_down(){
     if [ "$pid" != "" ]
     then
         kill -9 $pid
-        echo "Tale is stop!"
+        echo "my-sqlite-console is stop!"
     else
-        echo "Tale already stop!"
+        echo "my-sqlite-console already stop!"
     fi
 }
 
@@ -52,7 +55,7 @@ show_log(){
 }
 
 show_help(){
-    echo -e "\r\n\t欢迎使用Tale Blog"
+    echo -e "\r\n\t欢迎使用my-sqlite-console Blog"
     echo -e "\r\nUsage: sh my-sqlite-console.sh start|stop|reload|status|log"
     exit
 }
@@ -61,9 +64,9 @@ show_status(){
     pid=$(get_pid)
     if [ "$pid" != "" ]
     then
-        echo "Tale is running with pid: $pid"
+        echo "my-sqlite-console is running with pid: $pid"
     else
-        echo "Tale is stop!"
+        echo "my-sqlite-console is stop!"
     fi
 }
 
